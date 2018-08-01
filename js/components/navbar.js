824012c97d4684394e559ce319ad5b3018637419
tamybl <tamara.bravo.l@gmail.com>
2018-08-01 01:27:25 -0400

    add styles to menu

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -10,7 +10,7 @@ class NavBar extends HTMLElement {
         // Sirve para generar estilos propios
         // let navbarRoot = this.attachShadow({ mode: 'open' });
         this.innerHTML = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navigation">
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
@@ -23,11 +23,37 @@ class NavBar extends HTMLElement {
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- Menu Links -->
+        <!-- Primary Menu Panel -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <!-- Secondary Menu -->
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
+                    <a class="nav-link" href="#">Inicio
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
