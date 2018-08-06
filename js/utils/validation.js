a4cae8f87fadbc106512865eed75ea2aee066580
tamybl <tamara.bravo.l@gmail.com>
2018-08-06 13:06:43 -0400

    previsualizador de imagen del producto que evita subir enlaces rotos

--- js/utils/validation.js
+++ js/utils/validation.js
@@ -1,8 +1,5 @@
 function validationForm(form) {
     var nombre = form.name;
-    var marca = form.brand;
-    var estracto = form.description;
-    var pNormal = form.pnormal;
 
     const exp = /^\s+$/; // Expresion que revisa si hay solo espacios vacios
 
@@ -18,4 +15,37 @@ function validationForm(form) {
         return false;
     }
 
+}
+// Previsualizador de imagen de producto
+const btnPreview = document.getElementById('btnpreview');
+
+btnPreview.addEventListener('click', () => {
+    var image = document.getElementById('image');
+    const exp = /^\s+$/;
+    // Se crea
+    let containerImg = document.querySelector('.previewimage');
+    // Vaciar el contenedor de vista previa
+    containerImg.innerHTML = "";
+    // Se crea el elemento para ser renderizado
+    let img = document.createElement('img');
+    img.classList.add('img-fluid');
+    img.src = image.value;
+    if (fetchImage(img)) {
+        containerImg.appendChild(img);
+    }
+    else {
+        containerImg.innerHTML = "<p>La url de la imagen no es válida. Revisa e intenta nuevamente</p>";
+    }
+
+})
+
+// Funcion que revisa si la imagen se renderiza correctamente en el DOM (para evitar enlaces rotos)
+function fetchImage(img) {
+    if (img.complete) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    return true;
 }
