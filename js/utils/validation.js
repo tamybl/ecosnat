f0b7b178ebd67b46db962d5089e12a27176c564e
tamybl <tamara.bravo.l@gmail.com>
2018-08-06 22:49:46 -0400

    modify inputs to form and style button for validation step

--- js/utils/validation.js
+++ js/utils/validation.js
@@ -1,60 +1,77 @@
 function validationForm(form) {
-    var nombre = form.name;
+    let name = form.name;
+    let brand = form.brand;
+    let quantity = form.quantity;
+    let format = form.format;
+    let imageUrl = form.image;
+    let description = form.description;
+    let normalPrice = form.normalprice;
+    let internetPrice = form.internetprice;
+    let website = form.website;
 
     const exp = /^\s+$/; // Expresion que revisa si hay solo espacios vacios
 
     function valid(attr) {
-        if (attr == null || attr.value.lenght === 0 || exp.test(attr.value)) {
+        if (attr == null || attr.value.length === 0 || exp.test(attr.value)) {
             attr.style.border = '1px solid red';
         } else {
             attr.style.border = '1px solid green';
         }
     }
 
-    if (valid(nombre)) {
+    if (valid(name)) {
         return false;
     }
-
+    if (valid(brand)) {
+        return false;
+    }
+    if(valid(quantity)) {
+        return false;
+    }
+    if (valid(format)) {
+        return false;
+    }
 }
-// Previsualizador de imagen de producto
-const btnPreview = document.getElementById('btnpreview');
 
-btnPreview.addEventListener('click', () => {
-    var image = document.getElementById('image');
-    let containerImg = document.querySelector('.previewimage');
+
+// Carga Imagen de Producto y valida que el enlace funciona
+
+const btnUpload = document.getElementById('btnupload');
+
+btnUpload.addEventListener('click', () => {
+    const imageInput = document.getElementById('image');
+    const containerImg = document.querySelector('.previewimage');
+
     // Expresión que detecta si el link corresponde a la url de una imagen
     const exp = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|svg)/g;
-    
-    if (exp.test(image.value)) {
+
+    if (exp.test(imageInput.value)) {
         containerImg.innerHTML = "";
         // Se crea el elemento para ser renderizado
         let img = document.createElement('img');
-        img.classList.add('img-fluid');
-        img.src = image.value;
+        img.classList.add('img-fluid'); // <img src="url"
+        img.src = imageInput.value;
         // Si corresponde a un enlace roto, lanzara este evento de error
-        img.onerror = function () { 
+        img.onerror = function (err) {
+            console.log(err);
             containerImg.innerHTML = "<p>No se puede cargar la imagen.</p>";
-        }; 
-        
+        };
         let loader = document.createElement('img');
-            loader.src = 'img/loader.gif';
-            containerImg.appendChild(loader);
+        loader.src = 'img/loader.gif';
+        containerImg.appendChild(loader);
         // Si la imagen posee un link que funciona, gatillará el evento de onload que le dará tiempo a la imagen de cargar antes de arrojar error.
         img.onload = function () {
-            
+
             if (fetchImage(img)) {
-                image.style.border = '1px solid green';
+                imageInput.style.border = '1px solid green';
                 containerImg.replaceChild(img, loader);
             }
-
         };
     }
     else {
-        image.style.border = '1px solid red';
+        imageInput.style.border = '1px solid red';
         containerImg.innerHTML = "<p>La url de la imagen no es válida. Revisa e intenta nuevamente</p>";
     }
-
-
 })
 
 // Funcion que revisa si la imagen se renderiza correctamente en el DOM (para evitar enlaces rotos)
